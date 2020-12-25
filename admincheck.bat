97280dd428677643dbf8b631fc0bcd1ef400de7b
dbosle <32545222+dbosle@users.noreply.github.com>
2020-12-25 15:42:43 +0300

    Check run as Administrator

---
+++ admincheck.bat
@@ -0,0 +1,15 @@
+@Echo off
+
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Run this script as Administrator.
+        pause 
+        exit
+    )
